30aca93906bdb9f4d49c58a1de5bb0d3d138d0c0
Soklayy <soklayiter34@gmail.com>
2024-06-24 17:26:53 +0700

    Fix application crash issues

--- src/configs/mailler.config.ts
+++ src/configs/mailler.config.ts
@@ -5,9 +5,11 @@ export const mailerConfig = (config: ConfigService) => ({
     service: config.get<string>('EMAIL_SERVICE'),
     host: config.get('EMAIL_HOST'),
     port: +config.get('EMAIL_PORT'),
+    secure: true,
     auth: {
       user: config.get('EMAIL_USER'),
       pass: config.get('EMAIL_PASSWORD'),
     },
+    
   },
 });
